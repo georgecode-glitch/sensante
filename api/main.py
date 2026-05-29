5122d509de2a57dd0610d21055ad7eb7e054c0cf
mohamed aw <maw106277@gmail.com>
2026-05-29 06:52:33 +0000

    Fix missing imports StaticFiles FileResponse

--- api/main.py
+++ api/main.py
@@ -2,6 +2,8 @@
 # API FastAPI pour SenSante - Assistant pre-diagnostic medical
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
 import joblib
